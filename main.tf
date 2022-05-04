48a31cf9bc0eb3022fd9041048b22e951358e1fe
shaheena <you@example.com>
2022-05-04 11:37:02 +0000

    added

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "testing-s3-with-terraform1"
+   bucket = "testing-s3-with-terraform2"
    acl = "private"
    versioning {
       enabled = true
    }
+}
+
--- provider.tf
+++ provider.tf
@@ -1,3 +1,26 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIASUM6QXD3ZLCCHKG6"
+  secret_key = "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"
 }
+
+#terraform {
+ #   backend "s3" {
+  #  bucket = "testing-s3-with-terraform2"
+   # key    = "C:/Users/srupanagudi/Desktop/practice1/shaheena/terraform.tfstate"
+   # region = "us-east-1"
+    #}
+#}
+
+terraform {
+  cloud {
+    organization = "NOO"
+
+    workspaces {
+      name = "main"
+    }
+  }
+}
+
+
+
