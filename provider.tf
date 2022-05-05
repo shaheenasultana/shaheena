bf4251a8ef5743d78ad0f2d3cf31c986f46ede73
shaheenasultana <93178846+shaheenasultana@users.noreply.github.com>
2022-05-05 11:07:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+      required_providers {
+         aws = {
+         source = "hashicorp/aws"
+         version = "= 3.74.2"
+        }
+     }
+  }
 
 #terraform {
  #   backend "s3" {
